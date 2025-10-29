b8a860ca933f2c626b79a4e9e49ff2de3a0338e2
Violet Nguyen <nviolet0120@gmail.com>
2025-10-28 23:12:43 -0700

    Cleanup project structure, add babel config, setup supabase service

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
   },
 ]);
+module.exports = {
+  root: true,
+  extends: ["universe/native", "plugin:react/recommended"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+      alias: {
+        map: [["@env", "./"]],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
